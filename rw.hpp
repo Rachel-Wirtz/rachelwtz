1b5d70c7183b97d0ee1c3fcb848e367136e9a0f2
Rachel-Wirtz <info@rachel-wirtz.de>
2022-11-20 23:19:18 +0100

    Initial commit

---
+++ rw.hpp
@@ -0,0 +1,8 @@
+#ifndef RACHELWTZ__DEF__HPP
+#define RACHELWTZ__DEF__HPP
+
+#define RACHELWTZ_NAMESPACE     rw
+#define RACHELWTZ_BEGIN         namespace RACHELWTZ_NAMESPACE {
+#define RACHELWTZ_END            }
+
+#endif
