951a05eeb66c27db18301315e890632c0e38b26c
Rachel Wirtz <12850271+Rachel-Wirtz@users.noreply.github.com>
2022-11-21 16:57:27 +0100

    Initial Commit

---
+++ streams/rw-streams.cpp
@@ -0,0 +1,14 @@
+#define RACHELWTZ_TEST_STREAMS
+
+#include "rw-streams.hpp"
+#include "rw-testing.hpp"
+
+USING_RACHELWTZ;
+USING_RACHELWTZ_STREAMS;
+USING_RACHELWTZ_TESTING;
+
+int main() {
+    test test{};
+
+    return test.run();
+}
