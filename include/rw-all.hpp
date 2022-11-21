e2cca9ed5c56ca7e2cedaf35ce98f3529f9e846e
Rachel Wirtz <12850271+Rachel-Wirtz@users.noreply.github.com>
2022-11-21 17:34:10 +0100

    Initial Commit

---
+++ include/rw-all.hpp
@@ -0,0 +1,11 @@
+#ifndef RACHELWTZ__ALL__HPP
+#define RACHELWTZ__ALL__HPP
+
+#include "rw.hpp"
+#include "rw-core.hpp"
+#include "rw-concurrency.hpp"
+#include "rw-streams.hpp"
+#include "rw-strings.hpp"
+#include "rw-testing.hpp"
+
+#endif
